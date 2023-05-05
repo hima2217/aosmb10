50cd727b1f9f173d207ee888d1b4f1e2404243a0
Dany <lebedevdani2003@yandex.ru>
2023-05-05 23:58:39 +0300

    point 1

--- app/src/main/java/com/example/aosmb10/MainActivity.java
+++ app/src/main/java/com/example/aosmb10/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.aosmb10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void action(String doctor){
+        Log.d("доктор", "доктор: " + doctor);
+        Log.d("Лог", "Работает");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.execute(() -> {
+                action("Доктор 1");
+                action("Доктор 2");
+                action("Доктор 3");
+
+            });
+        executor.shutdown();}
     }
-}
